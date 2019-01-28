8d141131dd437b0eaa43487a2156a447483c70f4
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-28 19:37:06 +0100

    Koniec gry 2

--- adventure with python/adventure_with_python.py
+++ adventure with python/adventure_with_python.py
@@ -1,2 +1,31 @@
-print("przygoda z pythonem")
-input("\n\nAby zakonczyć program naciśnij klawisz Enter")
+print("Program 'Koniec gry' 2.0")
+
+print("Taki sam", "komunikat", "jak przedtem,")
+
+print("tylko",
+        "nieco",
+      "większy.")
+
+print("Oto", end=" ")
+print("on...")
+
+print(
+        """
+        _  __   ____    _   _   _____   ______    _____ 
+       | |/ /  / __ \  | \ | | |_   _| |  ____|  / ____|
+       | ' /  | |  | | |  \| |   | |   | |__    | |     
+       |  <   | |  | | | . ` |   | |   |  __|   | |     
+       | . \  | |__| | | |\  |  _| |_  | |____  | |____ 
+       |_|\_\  \____/  |_| \_| |_____| |______|  \_____|
+         
+         _____   _____   __     __
+       /  ____| |  __ \  \ \   / /
+       | |  __  | |__) |  \ \_/ / 
+       | | |_ | |  _  /    \   /  
+       | |__| | | | \ \     | |   
+        \_____| |_|  \_\    |_|  
+
+        """
+     )
+
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
