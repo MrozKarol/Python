9a5cc10e597934e5cf1a7a3bec670c40b7d40a24
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-29 09:49:29 +0100

    Maniepulations with quotes demonstrates chain methods

--- adventure with python/adventure_with_python.py
+++ adventure with python/adventure_with_python.py
@@ -1,7 +1,19 @@
-#Osobisty pozdrawiacz
+#Maniulacje cytatami
 
-neme = input("Witaj jak masz na imie?")
-print(neme)
-print("Cześć", neme)
+quote ="Myślę ze jezyk programowania Python jest bardziej przemyślany niż JavaScrpt "
+print("Moja opinia")
+print(quote)
+
+print("\nDużymi literami:")
+print(quote.upper()) 
+
+print("\nMałymi literami:")
+print(quote.lower()) 
+
+print("\nWszystkie wyrazy od duzej litery:")
+print(quote.title()) 
+
+print("\nZ dobrą zmianą:")
+print(quote.replace("przemyślany", "zmyślny".upper()))
 
 input("\n\n\t\tAby zakonczyć program, nacisnij klawisz enter")
