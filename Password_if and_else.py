1eec47e1c3452b976ffdfdeee9f238266669b644
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-29 11:20:09 +0100

    if

---
+++ Password_if and_else.py
@@ -0,0 +1,12 @@
+#Password
+#Demonstruje instrukcje if
+
+print("Witaj w super skomolikowanym programie password")
+print("--bezpieczeństwo to podstwa szczacunek tez\n")
+
+password = input("Wprowadź hasło:\n")
+if password == "tetriandoh":
+    print("Dostep został udzielony")
+
+
+input("\n\n\t\tAby zakonczyć program, nacisnij klawisz enter")
