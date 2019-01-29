83a59adad9ca16c6b6901f31d465d171b5771229
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-29 09:30:22 +0100

    Personal greetings Demonstrates downloading of data entered by the user

--- adventure with python/adventure_with_python.py
+++ adventure with python/adventure_with_python.py
@@ -1,5 +1,7 @@
-# Pozdrawiacz JOŁ Joł;
-name = "BELZEBUB"
-print(name)
-print("CZEŚĆ", name)
-input("\n\nt\Aby zakonczyć program, naciśnij klawisz Enter")
+#Osobisty pozdrawiacz
+
+neme = input("Witaj jak masz na imie?")
+print(neme)
+print("Cześć", neme)
+
+input("\n\n\t\tAby zakonczyć program, nacisnij klawisz enter")
