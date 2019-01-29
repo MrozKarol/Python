94e2174233fa6b1792b8e6c250495ba2cc67f1d5
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-29 13:28:05 +0100

    using the break function and the continue function

---
+++ adventure with python/break_and_continue_ strange_counter.py
@@ -0,0 +1,9 @@
+count = 0
+while True:
+    count +=1
+    if count >10:
+        break
+    if count == 5:
+         continue    
+    print(count)
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")    
