09c29687153f14cbae4f2595236b4277a569834c
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-29 11:50:23 +0100

    double if

--- Password_if and_else.py
+++ Password_if and_else.py
@@ -7,6 +7,8 @@ print("--bezpieczeństwo to podstwa szczacunek tez\n")
 password = input("Wprowadź hasło:\n")
 if password == "tetriandoh":
     print("Dostep został udzielony")
+if password != "tetriandoh":
+        print("złe hasło")    
 
 
 input("\n\n\t\tAby zakonczyć program, nacisnij klawisz enter")
