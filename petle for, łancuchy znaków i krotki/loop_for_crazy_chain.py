0ba67bc4d9055133da80e75e4bf21bc01acce50d
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-02-01 18:08:12 +0100

    Loop for deminstrates the use of a fot loop witch a character string

---
+++ petle for, łancuchy znaków i krotki/loop_for_crazy_chain.py
@@ -0,0 +1,9 @@
+# Zwariowany łańcuch
+# Demonstruje użycie pętli for z łańcuchem znaków
+word = input("Wprowadź jakieś słowo (tylko ładne :p): ")
+
+print("\nOto poszczególne litery Twojego słowa:")
+for janusz in word:
+    print(janusz)
+
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
