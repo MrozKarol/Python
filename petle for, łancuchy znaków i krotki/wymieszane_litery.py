2a69e22cc18a4375e676ef44e62f7c8bc5cf4b01
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-02-07 22:38:13 +0100

    WIP: wymieszane litery use choice() function

--- petle for, łancuchy znaków i krotki/wymieszane_litery.py
+++ petle for, łancuchy znaków i krotki/wymieszane_litery.py
@@ -2,3 +2,17 @@
 # komputer wybiera losowo słowo a potem miesza w nim litery
 # gracz powinien odgadnąć w niem pierwotne słowo
 
+import random
+
+# utworzenie specjalnej zmiennej i przypisanie jej krotki
+
+WORDS = ("python", "javascript", "git", "konglomerat", "odpowiedź")
+
+# wybierz losowo jedno słowo z sekwencji
+
+word = random.choice(WORDS)
+
+print(word)
+
+#utwórz zmienną by później uzyć jej do sprawdzenia czy odpowiedź jest poprawna
+correct = word
