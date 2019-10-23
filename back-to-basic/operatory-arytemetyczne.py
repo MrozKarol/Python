71216e81257ffb710fb076cbe38bfd5fb47f22fd
MrozKarol <polityk357@gmail.com>
2019-10-23 18:25:48 +0200

    displaying the gross price for the course

---
+++ back-to-basic/operatory-arytemetyczne.py
@@ -0,0 +1,27 @@
+
+"""
+
++ dodawanie
+- odejmowanie
+* mnozenie
+/ dzielenie
+
+() jak w matematyce kieruje kolejnoscia dzialania
+// dzielenie w dol
+% - modulo reszta z dzielenia
+
+"""
+#program liczacy ceny brutto za kurs
+
+VAT = 23
+obliczonyVAT = (1 + VAT / 100)
+cenaNettoJava = 10
+cenaNettoAjax = 20
+
+cenaBruttoJava = cenaNettoJava * obliczonyVAT
+cenaBruttoAjax = cenaNettoAjax * obliczonyVAT
+
+
+
+print('cenaBruttoJava' ,cenaBruttoJava)
+print('cenaBruttoAjax', cenaBruttoAjax)
