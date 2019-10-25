8c8aa913c9300dd65059c1f9b0d737ec358a83b6
MrozKarol <polityk357@gmail.com>
2019-10-25 19:26:38 +0200

    added simple plus calculator

--- back-to-basic/funkcje.py
+++ back-to-basic/funkcje.py
@@ -6,3 +6,12 @@ print(sqrt(4))
 imie= 'karol'
 
 print(imie.capitalize())
+
+print('Kalkulator który tylko dodaje')
+# print('Wpisz wartość a')
+a = int(input('Wpisz wartość a '))
+
+# print('Wpisz wartość b')
+b = int(input('Wpisz wartość b '))
+suma = a + b
+print('Wynik dodania a + b : ',  str(suma))
