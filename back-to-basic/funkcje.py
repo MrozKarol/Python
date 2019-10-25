7ab35af61d9c1022e648705e69ead51e1c3e9206
MrozKarol <polityk357@gmail.com>
2019-10-25 19:43:39 +0200

    name and age program

--- back-to-basic/funkcje.py
+++ back-to-basic/funkcje.py
@@ -1,17 +1,29 @@
-# import math
-from math import sqrt
+# # import math
+# from math import sqrt
 
-print(sqrt(4))
+# print(sqrt(4))
 
-imie= 'karol'
+# imie= 'karol'
 
-print(imie.capitalize())
+# print(imie.capitalize())
 
-print('Kalkulator który tylko dodaje')
-# print('Wpisz wartość a')
-a = int(input('Wpisz wartość a '))
+# print('Kalkulator który tylko dodaje')
+# # print('Wpisz wartość a')
+# a = int(input('Wpisz wartość a '))
 
-# print('Wpisz wartość b')
-b = int(input('Wpisz wartość b '))
-suma = a + b
-print('Wynik dodania a + b : ',  str(suma))
+# # print('Wpisz wartość b')
+# b = int(input('Wpisz wartość b '))
+# suma = a + b
+# print('Wynik dodania a + b : ',  str(suma))
+
+print('Jak masz na imie ?')
+
+name = input('podaj imie :')
+
+print('Witaj', name, 'ile masz lat ?' )
+
+age = int(input('wpisz wiek '))
+
+agePlusYear = age + 1 
+
+print(name, 'za rok będziesz miał', str(agePlusYear), 'lat')
