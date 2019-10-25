5b86dafa0a27801f3d163ef187415134d89270fa
MrozKarol <polityk357@gmail.com>
2019-10-25 18:15:12 +0200

    how to use stings

---
+++ back-to-basic/stringi.py
@@ -0,0 +1,14 @@
+imie = 'karol'
+nazwisko = 'mroz'
+pelne = imie+' '+nazwisko
+
+print(pelne)
+
+dlugiTekst = ' lorem lorem lorem lorem lorem lorem lorem\
+     lorem lorem loremlorem loremlorem loremlorem \
+         loremlorem loremlorem lorem'
+print(dlugiTekst)  
+
+imieTwo = 'Kamila'
+
+print(imieTwo[-1])
