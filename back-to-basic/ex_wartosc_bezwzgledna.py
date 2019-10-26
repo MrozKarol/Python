d56e9ea071e6eaf2049d09079519be1d4e2a09cd
MrozKarol <polityk357@gmail.com>
2019-10-26 14:59:08 +0200

    added the absolute value

---
+++ back-to-basic/ex_wartosc_bezwzgledna.py
@@ -0,0 +1,16 @@
+# spaw aby liczba ujemna stałą sie dodatnia
+
+
+print('Program obliczna wartość bezwzgledna')
+
+x = int(input('Podaj liczbę: '))
+
+if(x < 0):
+    print('Wartość bezwzgledna z ', str(x), 'to', str(x * -1))
+elif (x > 0):
+    print('Wartość bezwzgledna z ', str(x), 'to', str(x *  1))
+elif (x == 0):
+     print('Wartość bezwzgledna z ', str(x), 'to', str(x *  1))
+
+
+
