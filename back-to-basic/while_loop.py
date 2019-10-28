eea4ece6e3d61a39b7b5e6075d11636afe8fe9fd
MrozKarol <polityk357@gmail.com>
2019-10-28 19:20:56 +0100

    the program asks how many numbers you want to sum up, simple use of while loops

--- back-to-basic/while_loop.py
+++ back-to-basic/while_loop.py
@@ -1,7 +1,21 @@
 # petla
 
-number =  int(input('Wpisz liczbę która zmniejszy sie do 0: '))
+# number =  int(input('Wpisz liczbę która zmniejszy sie do 0: '))
 
-while number >= 0:
-    print(number)
-    number -=1
+# while number >= 0:
+#     print(number)
+#     number -=1
+
+# ćwiczenie jeden: program pyta ile chcesz zsumować ze sobą liczb
+
+wynik = 0
+i = 0
+
+xSum = int((input('podaj ile chcesz zsumować ze sobą liczb: ')))
+
+while i < xSum:
+    x = int(input('Podaj liczbę: '))
+    wynik += x
+    i += 1
+
+print("Wynik dodawania liczn to:", wynik)   
