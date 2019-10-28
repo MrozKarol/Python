1881ed92c9f72921f2ec273c9683636932e8d290
MrozKarol <polityk357@gmail.com>
2019-10-28 19:47:30 +0100

    example 3 how to use a loop for: The program prints numbers from 0 to 200 that are divisible by 5 and not divisible by 7 '

--- back-to-basic/for_loop.py
+++ back-to-basic/for_loop.py
@@ -1,7 +1,17 @@
-wynik = 0
+# wynik = 0
 
-for i in range(0,200):
-   if( i % 2 == 0):
-       print('Liczba', i, 'jest parzysta')
+# for i in range(0,200):
+#    if( i % 2 == 0):
+#        print('Liczba', i, 'jest parzysta')
 
-print("Wynik dodawania liczb to:", wynik)    
+# print("Wynik dodawania liczb to:", wynik)    
+
+# wypisz liczby od 0 do 200 które są podzielne przez 5 i nie są podzielne przez 7
+
+print('Program wypisje liczny liczby od 0 do 200 które są podzielne przez 5 i nie są podzielne przez 7 ')
+
+for i in range(0,201):
+    if( i % 5 == 0):
+        print('Liczby podzielne przez 5 to:', i)  
+    elif(not(i % 7 == 0)):
+        print('Liczby które nie są podzielne przez 7 to:', i)  
