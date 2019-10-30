73393f2e53771128fae92595a8f3fae729485f72
MrozKarol <polityk357@gmail.com>
2019-10-30 20:47:26 +0100

    added program that adds three even numbers

---
+++ back-to-basic/ex/dodawanie_liczb_parzystych.py
@@ -0,0 +1,22 @@
+# Napisz program, który doda kolejne 3 parzyste liczby dodatnie podane przez użytkownika
+
+
+wynik = 0
+i=0
+
+while i < 3:
+    x = int(input('Podaj liczbę parzysta: '))
+    if (x%2 == 0):
+        wynik += x
+    else:
+        wybor = str(input('Miałabyć być liczba parzysta, wyśnij a żeby kontynuować b żeby przerwać: '))
+        if(wybor == 'a'):
+            continue
+        elif (wybor == 'b'):
+            print('Koniec programu')
+            break
+    i += 1
+print('Wynik to:', wynik, 'z', i, 'dodań') 
+
+
+   
