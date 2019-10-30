ae5c721fea4afa530c992aa9a1199f490a6a419a
MrozKarol <polityk357@gmail.com>
2019-10-30 20:02:40 +0100

    example how to use continue

--- back-to-basic/break_continue.py
+++ back-to-basic/break_continue.py
@@ -1,16 +1,17 @@
 # break
 
 wynik = 0 
-
-for i in range(0,3):
+i = 0
+while i < 3:
     x = int(input('Podaj dodatnią liczbę'))
     if(x>0):
         wynik += x
         print(wynik)
     else:
         print('Miała być liczba dodatnia')    
-        break
+        # break
+        continue
     print('Aktualny wynik dodawania to :', wynik)
-        
+    i +=1    
     
 
