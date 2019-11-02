95b893fedb7a36cd658f8df0f27b149cd3f0be74
MrozKarol <polityk357@gmail.com>
2019-11-02 17:11:46 +0100

    added new search terms

--- back-to-basic/ex/Zgadnij liczbę.py
+++ back-to-basic/ex/Zgadnij liczbę.py
@@ -8,15 +8,10 @@ while i < 10:
     x = int(input('Podaj liczbę : '))
     i += 1   
     print('Liczba prób', i)    
-    if(x == szukanaLiczba):
+    if (x == szukanaLiczba):
         print("Brawo odnalzałeś liczbę !!! szukana liczba to", str(szukanaLiczba))
         break
-    elif( (szukanaLiczba - x) < 5 and (x - szukanaLiczba)< 5):
-        print('Jesteś blisko')
-    # elif( (szukanaLiczba - x) < 10):
-    #     print('To Twoj obszar poszukiwań')    
-    else:
-        if(i ==10):
+    elif (i == 10):
             print("Nie udało Ci sie odgadnąć liczby przekroczyłeś maksymalną liczbę prób")
             wybor = str(input('naciśj a żeby zakończyć b żeby spróbować jeszcze raz'))
             if(wybor == 'a'):
@@ -25,6 +20,22 @@ while i < 10:
             elif (wybor == 'b'):
                 break
             print('Koniec programu')
+    elif ((szukanaLiczba - x) < 5 and (x - szukanaLiczba)< 5):
+            print('Jesteś blisko')
+    elif ((szukanaLiczba - x) < 10 and (x - szukanaLiczba)< 10):
+            print('Ooo ciepło to Twoj obszar poszukiwań')
+    elif ((szukanaLiczba - x) < 20 and (x - szukanaLiczba)< 20):
+            print('Nie jesteś nawet blisko')    
+    # else:
+    #     # if(i == 10):
+    #     #     print("Nie udało Ci sie odgadnąć liczby przekroczyłeś maksymalną liczbę prób")
+    #     #     wybor = str(input('naciśj a żeby zakończyć b żeby spróbować jeszcze raz'))
+    #     #     if(wybor == 'a'):
+    #     #         i = 0
+    #     #         continue
+    #     #     elif (wybor == 'b'):
+    #     #         break
+    #     #     print('Koniec programu')
     
      
         
