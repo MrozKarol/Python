ae2ffd428044a8a81be7738ba7542d11688d81d2
MrozKarol <polityk357@gmail.com>
2019-11-03 14:39:37 +0100

    downloaded value from dictionary

---
+++ back-to-basic/krotki.py
@@ -0,0 +1,18 @@
+krotka = (1,2,3,4)
+
+print(krotka)
+
+# dictionary - słownik KLUCZ WARTOŚĆ
+
+pokoje = {49: 'Karol M',
+          69: 'Beta Z',
+          10: 'Janina J'}
+print(pokoje[49])
+
+pokoje[49] = 'Stefan S'
+
+print(pokoje[49])
+
+pokoje[50] = 'Barbara BB'
+
+print(pokoje)
