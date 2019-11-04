51b567961744767db7202f0ff1e97c2d66ca98fa
MrozKarol <polityk357@gmail.com>
2019-11-04 19:55:12 +0100

    display nested type: three tuple inside list

---
+++ back-to-basic/petle a typy zagniezdzone przetwarzanie danych.py
@@ -0,0 +1,12 @@
+listaGosci= {
+                  ('Arkadiusz', 29, 'mężczyzna'),
+                  ('Wioletta', 22, 'kobieta'),
+                  ('Kuba', 33, 'mężczyzna')
+
+            }
+
+for imie, wiek, plec in listaGosci:
+    print("Imie: ", imie)
+    print("Wiek : ", wiek)
+    print("Płeć : ", plec)
+    print('\n')
