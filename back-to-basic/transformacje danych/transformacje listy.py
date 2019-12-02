890c8112fdfc1da506fa762bb08ae614cd3df449
MrozKarol <polityk357@gmail.com>
2019-12-02 17:47:24 +0100

    adding a comment

--- back-to-basic/transformacje danych/transformacje listy.py
+++ back-to-basic/transformacje danych/transformacje listy.py
@@ -13,3 +13,20 @@ for element in liczby:
     if(element %2 == 0):
         liczbyParzyste.append(element)
 print(liczbyParzyste)        
+
+# wyrazenie listowne przykład :
+potegiDwojki2 = [element**2 for element in liczbyParzyste]
+
+print(potegiDwojki)
+
+# wyrazenie listowne przykład2 z uzyciem enter zeby kod był lepiej czytelny :
+
+liczbyParzyste2 = [element
+                    for element in liczby
+                    if(element %2 == 0)
+                    ]
+print(liczbyParzyste2)  
+
+"""
+[co_zrobic_na_elemencie | for element in stara_lista | warunek_oparty_na_elemencie]
+"""
