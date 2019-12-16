34b81ace3fbd386b91e3adefc8f49b4babdc1b4a
MrozKarol <polityk357@gmail.com>
2019-12-16 18:19:56 +0100

    added another two solution

--- back-to-basic/funkcje/Suma Liczb.py
+++ back-to-basic/funkcje/Suma Liczb.py
@@ -3,20 +3,33 @@ Napisz program który policzy sumę wszystkich liczb od 1 do
 do podanej liczby
 """
 
+
 def sumuj_do(liczba):
     suma = 0
     for liczba in range(1, liczba + 1):
         suma = suma + liczba
     return suma
 
+
 def sumuj_do2(liczba):
     
     return sum([liczba
         for liczba in range(1, liczba + 1)
-
     ])
-
+  
 def sumuj_do3(liczba):
+    
+    return sum({liczba
+        for liczba in range(1, liczba + 1)
+    })
+
+def sumuj_do4(liczba):
+    
+    return sum((liczba
+        for liczba in range(1, liczba + 1)
+    ))
+
+def sumuj_do5(liczba):
      return int(((1 + liczba) / 2 * liczba))    
 
 
@@ -25,4 +38,8 @@ print(sumuj_do(5))
 print(sumuj_do2(5))
 
 print(sumuj_do3(5))
+
+print(sumuj_do4(5))
+
+print(sumuj_do5(5))
 
