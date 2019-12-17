85f22845f17195b69b3771bab1678a8eb1633423
MrozKarol <polityk357@gmail.com>
2019-12-17 19:42:46 +0100

    compare time difference solution by time.perf_counter

--- back-to-basic/funkcje/Suma Liczb.py
+++ back-to-basic/funkcje/Suma Liczb.py
@@ -2,7 +2,11 @@
 Napisz program który policzy sumę wszystkich liczb od 1 do
 do podanej liczby
 """
+"""
+Mierzenie wydajności kodu
+"""
 
+import time
 
 def sumuj_do(liczba):
     suma = 0
@@ -32,14 +36,30 @@ def sumuj_do4(liczba):
 def sumuj_do5(liczba):
      return int(((1 + liczba) / 2 * liczba))    
 
-
+start = time.perf_counter()
 print(sumuj_do(5))
+end = time.perf_counter()
+print(end-start)
 
+start = time.perf_counter()
 print(sumuj_do2(5))
+end = time.perf_counter()
+print(end-start)
 
+start = time.perf_counter()
 print(sumuj_do3(5))
+end = time.perf_counter()
+print(end-start)
 
+
+start = time.perf_counter()
 print(sumuj_do4(5))
+end = time.perf_counter()
+print(end-start)
 
+
+start = time.perf_counter()
 print(sumuj_do5(5))
+end = time.perf_counter()
+print(end-start)
 
